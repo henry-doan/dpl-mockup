35ecb8f0255e4924776ca09b0a1922d6deacdaf3
Henry Doan <henrydoan96@gmail.com>
2016-11-30 14:06:03 -0700

    added repack and printed hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  # KEEP THIS AT BOTTOM
+  get '*unmatched_route', to: 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
